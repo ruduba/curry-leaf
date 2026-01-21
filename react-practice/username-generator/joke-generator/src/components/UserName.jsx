9e64313a11aa85158bee3f7af97780f81650079b
ruduba <vatbudur@gmail.com>
2026-01-21 13:15:12 +0530

    Restructured divs & layout

--- react-practice/username-generator/joke-generator/src/components/UserName.jsx
+++ react-practice/username-generator/joke-generator/src/components/UserName.jsx
@@ -4,18 +4,41 @@ import "./component.css"
 
 const UserName = () => {
 
-const [username, setUsername] = useState("Username Goes Here");
+const [username1, setUsername1] = useState("");
+const [username2, setUsername2] = useState("");
 
+const fetch1 = () => {
+            
+            fetch("https://word-generator-api.adriencarpentier.com/en/word/get")
+            .then((res) => res.json())
+            .then((data) => setUsername1(data))
+            
+        }
 
-
+const fetch2 = () => {
+            fetch(" https://random-word-api.herokuapp.com/word?number=1")
+            .then((res) => res.json())
+            .then((data) => setUsername2(data))
+        }
 
 return(
+    < >
+    <div className="fix">
+            <div className="username-here">
 
-    <div className="username-here">
         
-        <p>{username}</p>
 
+        <p>{username1+username2}</p>
+                
     </div>
+    <div className="btn">
+        <Button callApi = {fetch2}/>
+       </div>
+
+        
+    </div>
+
+    </>
 
 
 
