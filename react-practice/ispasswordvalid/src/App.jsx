030fadc3b2dcb4f57de3f20804323eee230b3503
ruduba <vatbudur@gmail.com>
2026-01-22 18:07:34 +0530

    Functionality added, working validator

--- react-practice/ispasswordvalid/src/App.jsx
+++ react-practice/ispasswordvalid/src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from 'react'
-
+import validator from 'validator'
 import './App.css'
 
 function App() {
-  const[password, setPassword] = useState("");
+    const[strength, setStrength] = useState("lets go");
 
+    const validate = (value) => {
+
+      if(validator.isStrongPassword(value, {
+        minLength: 8, minLowercase:2, minUppercase:2, minNumbers: 2, minSymbols: 2
+      })) {
+        setStrength("Is Strong! Good to GO")
+      }else{
+        setStrength("Is weak, try a stronger password")
+      }
+    }
   return (
     <>
       
@@ -12,13 +22,15 @@ function App() {
 
       <h1>Enter Password</h1>
 
-      <input type='password'
+      <input type='text'
       placeholder='enter here'
+      style={{border: strength === "Is Strong! Good to GO" ? "2px dotted green" : "2px dotted red"}}
+      onChange={(e) => validate(e.target.value)}
       > 
       </input>
       
-      <p>
-        
+      <p style={{color: strength === "Is Strong! Good to GO" ? "green" : "red"}}>
+        {strength}
       </p>
 
 
